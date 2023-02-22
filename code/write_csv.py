a0557eb8d8f7ed4a2c257091b97501c60d7f8a20
Nathaniel Robinson <n8rrobinson@gmail.com>
2023-02-22 02:22:55 -0500

    fix to write pickle to save data

---
+++ code/write_csv.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import pickle as pkl
+
+def pkl2csv(no2voice, no2prompt, out_csv):
+    with open(out_csv, 'w') as f:
+        for no in no2voice:
+            f.write(f"{no}, {no2voice[no]}, {no2prompt[no]}\n")
+    print(f"Written {len(no2voice)} mappings to {args.out_csv}", flush=True)
+    return
+
+def main(args):
+
+    with open(args.in_pkl, 'rb') as f:
+        no2voice, no2prompt = pkl.load(f)
+
+    csv_dir = os.path.split(args.out_csv)[0]
+    if not os.path.exists(csv_dir):
+        os.makedirs(csv_dir)
+
+    pkl2csv(no2voice, no2prompt, args.out_csv)
+    return
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--in-pkl", type=str, required=True,\
+            help="Path to mappings pickle")
+    parser.add_argument("--out-csv", type=str, required=True,\
+            help="Path to output csv")
+
+    args = parser.parse_args()
+
+    main(args)
